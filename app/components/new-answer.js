bbab78c98da4c8811e9894654dbef5a091eee846
Calla Rudolph <callarudolph@gmail.com>
2017-08-11 12:08:40 -0700

    build out components to save new answers with one to many relationship

--- app/components/new-answer.js
+++ app/components/new-answer.js
@@ -1,4 +1,21 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  addNewAnswer: false,
+  actions: {
+    answerFormShow() {
+      this.set('addNewAnswer', true);
+    },
+    saveAnswer() {
+      var params = {
+        response: this.get('response') ? this.get('response') : "",
+        responder: this.get('responder') ? this.get('responder') : "",
+        question: this.get('question')
+      };
+      this.set('addNewAnswer', false);
+      this.set('response', "");
+      this.set('responder', "");
+      this.sendAction('saveAnswer', params);
+    }
+  }
 });
